b8ff91039a67ee8b3d315edf4b59bbd4ed74ce33
Marthox1999 <mateo.gregory@correounivalle.edu.co>
2019-03-17 10:53:43 -0500

    ejemplos de las peticiones Get Post y Put

--- ExpressBases/server.js
+++ ExpressBases/server.js
@@ -4,14 +4,72 @@ const port = 3000
 const pgp = require('pg-promise')(/*options*/)
 const db = pgp('postgres://postgres:postgres@localhost:5432/postgres')
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// GET REQUESTS
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/', (req, res) => {
+  res.send('I am a get request!')
+})
 
-db.one('SELECT $1 AS value', 123)
+app.get('/users', (req, res) => {
+  db.one('SELECT $1 AS value', 123)
   .then(function (data) {
     console.log('DATA:', data.value)
+    res.send(JSON.stringify(data.value))
   })
   .catch(function (error) {
     console.log('ERROR:', error)
   })
+})
+
+app.get('/users/:userid', (req, res) => {
+  console.log(req.params.userid)
+  res.send(req.params.userid)
+})
+
+//POST REQUESTS
+
+app.post('/', (req, res) =>{
+  res.send("I am a post request!")
+})
+
+app.post('/users', (req, res) =>{
+  db.one('SELECT $1 AS value', 123)
+  .then(function (data) {
+    console.log('DATA:', data.value)
+    res.send(JSON.stringify(data.value))
+  })
+  .catch(function (error) {
+    console.log('ERROR:', error)
+  })
+})
+
+app.post('/users/:newuser', (req, res) =>{
+  console.log(req.params)
+  res.send(JSON.stringify(req.params.newuser))
+})
+
+//PUT REQUESTS
+
+app.put('/', (req, res) =>{
+  res.send("I am a put request!")
+})
+
+app.put('/users', (req, res) =>{
+  db.one('SELECT $1 AS value', 123)
+  .then(function (data) {
+    console.log('DATA:', data.value)
+    res.send(JSON.stringify(data.value))
+  })
+  .catch(function (error) {
+    console.log('ERROR:', error)
+  })
+})
+
+app.put('/users/:newuser', (req, res) =>{
+  console.log(req.params)
+  res.send(JSON.stringify(req.params.newuser))
+})
+
+//LISTENER
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
